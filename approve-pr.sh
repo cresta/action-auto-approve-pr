917e4bc6423e9ccd1b7dbb639937a269b4543f97
Jack Lindamood <jack@cresta.ai>
2023-05-03 11:39:20 -0700

    fix: for non empty vars

--- approve-pr.sh
+++ approve-pr.sh
@@ -2,17 +2,12 @@
 set -exuo pipefail
 ARGS_TO_EXEC=()
 
-if [ -n "${GITHUB_REPOSITORY}" ]; then
+if [ -n "${GITHUB_REPOSITORY-}" ]; then
   ARGS_TO_EXEC+=("--repo")
   ARGS_TO_EXEC+=("${GITHUB_REPOSITORY}")
 else
   echo "GITHUB_REPOSITORY is not set. Exiting..."
   exit 1
-fi
-
-if [ -n "${INPUT_COMMENT}" ]; then
-  ARGS_TO_EXEC+=("--comment")
-  ARGS_TO_EXEC+=("${INPUT_COMMENT}")
 fi
 
 if [ -n "${INPUT_BODY}" ]; then
